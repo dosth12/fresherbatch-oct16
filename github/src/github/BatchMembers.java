7abd3db6495e3f55c8883aa66ba52f6cc0a36697
ahammad <ahammadbhasha103@gmail.com>
2022-05-14 13:01:26 +0530

    first commit

---
+++ github/src/github/BatchMembers.java
@@ -0,0 +1,20 @@
+package github;
+
+public class BatchMembers {
+      static String i="Rohith";
+      static String j="Ahammad";
+      static String k="Durga";
+      static String l="Kushal";
+      static String m="Karimulla";
+      
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("My GL is "+i);
+   System.out.println("Team members");
+   System.out.println(j);
+   System.out.println(k);
+   System.out.println(l);
+   System.out.println(m);
+	}
+
+}
